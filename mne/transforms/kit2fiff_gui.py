ff634dfb9412ab4c34512506967908c249ef5f15
Christian Brodbeck <christianmbrodbeck@gmail.com>
2013-03-03 02:45:27 -0500

    kit2fiff_gui

--- mne/transforms/kit2fiff_gui.py
+++ mne/transforms/kit2fiff_gui.py
@@ -8,30 +8,30 @@ import cPickle as pickle
 import os
 
 from mayavi.core.ui.mayavi_scene import MayaviScene
-from mayavi.mlab import pipeline
 from mayavi.tools.mlab_scene_model import MlabSceneModel
 import numpy as np
 from pyface.api import confirm, error, FileDialog, OK, YES
-from traits.api import HasTraits, on_trait_change, Instance, Property, \
+from traits.api import HasTraits, HasPrivateTraits, cached_property, on_trait_change, Instance, Property, \
                        Array, Bool, Button, Color, Enum, File, Float, Int, List, \
                        Range, Str, Tuple
 from traitsui.api import View, Item, Group, HGroup, VGroup, CheckListEditor
 from traitsui.menu import NoButtons
 from tvtk.pyface.scene_editor import SceneEditor
 
-from .marker_gui import MarkerPanel, MarkerPointDest
+from .marker_gui import MarkerPanel
 from .coreg import fit_matched_pts
 from .transforms import apply_trans, coord_trans
 from .viewer import HeadViewController, PointObject
 from ..fiff.kit.coreg import read_hsp, read_elp, transform_ALS_to_RAS, \
                              get_neuromag_transform
+from ..fiff.kit.kit import RawKIT
 
 
 
 use_editor = CheckListEditor(cols=5, values=[(i, str(i)) for i in xrange(5)])
 
 
-class Kit2FiffPanel(HasTraits):
+class Kit2FiffPanel(HasPrivateTraits):
     """Control panel for kit2fiff conversion"""
     # Source Files
     sqd_file = File(filter=['*.sqd'])
@@ -41,24 +41,21 @@ class Kit2FiffPanel(HasTraits):
 
     # Marker Points
     mrk_ALS = Array(float, shape=(5, 3))
-    mrk = Property(Array(float, shape=(5, 3)), depends_on=('mrk_ALS'))
+    mrk = Property(depends_on=('mrk_ALS'))
     use_mrk = List(range(5), desc="Which marker points to use for the device "
                    "head coregistration.")
 
-    # Head Shape Fiducials
-    elp_raw = Property(Array(float, shape=(8, 3)), depends_on=['fid_file'])
-    neuromag_trans = Property(Array(float, shape=(3, 3)),
-                              depends_on=['elp_raw'])
-    elp_src = Property(Array(float, shape=(5, 3)), depends_on=['neuromag_trans'])
-    fid_src = Property(Array(float, shape=(3, 3)), depends_on=['neuromag_trans'])
-    dev_head_trans = Property(Array(float, shape=(4, 4)),
-                              depends_on=['elp_src', 'use_mrk', 'mrk'])
-    elp_dst = Property(Array(shape=(5, 3)), depends_on=['dev_head_trans'])
-    fid_dst = Property(Array(shape=(3, 3)), depends_on=['dev_head_trans'])
+    # Polhemus Fiducials
+    elp_raw = Property(depends_on=['fid_file'])
+    hsp_raw = Property(depends_on=['hsp_file'])
+    neuromag_trans = Property(depends_on=['elp_raw'])
 
-    # Head Shape
-    hsp_src = Property(Array(float, shape=(None, 3)), depends_on=['hsp_file'])  # hsp in neuromag
-    hsp_dst = Property(Array(float, shape=(None, 3)), depends_on=['hsp_src', 'dev_head_trans'])  # hsp in neuromag
+    # Polhemus data
+    elp_src = Property(depends_on=['neuromag_trans'])
+    fid_src = Property(depends_on=['neuromag_trans'])
+    hsp_src = Property(depends_on=['hsp_raw', 'neuromag_trans'])
+
+    dev_head_trans = Property(depends_on=['elp_src', 'use_mrk', 'mrk'])
 
     # Events
     events = Array(Int, shape=(None,), value=[])
@@ -68,61 +65,81 @@ class Kit2FiffPanel(HasTraits):
     # Visualization
     scene = Instance(MlabSceneModel)
     fid_obj = Instance(PointObject)
+    elp_obj = Instance(PointObject)
     hsp_obj = Instance(PointObject)
-#    sns_obj= Instance(PointObject)
+
+    # Output
+    can_save = Property(Bool, depends_on=['sqd_file', 'elp_dst', 'hsp_dst',
+                                          'dev_head_trans'])
+    save_as = Button(enabled_when='can_save', label='Save FIFF...')
 
     view = View(VGroup(VGroup(Item('sqd_file', label="Data"),
-                              Item('hsp_file', label='Head Shape'),
                               Item('fid_file', label='Dig Points'),
+                              Item('hsp_file', label='Head Shape'),
                               Item('use_mrk', editor=use_editor, style='custom'),
                               label="Sources", show_border=True),
                        VGroup(Item('endian', style='custom'),
                               Item('event_info', style='readonly', show_label=False),
                               label='Events', show_border=True)))
 
+    @cached_property
     def _get_mrk(self):
         return transform_ALS_to_RAS(self.mrk_ALS, unit='m')
 
+    @cached_property
     def _get_elp_raw(self):
         if os.path.exists(self.fid_file):
             pts = read_elp(self.fid_file)
             return pts
-        else:
-            return np.empty((0, 3))
 
+    @cached_property
+    def _get_hsp_raw(self):
+        if os.path.exists(self.hsp_file):
+            return read_hsp(self.hsp_file)
+
+    @cached_property
     def _get_neuromag_trans(self):
-        if np.any(self.elp_raw):
-            pts = transform_ALS_to_RAS(self.elp_raw[:3])
-            nasion, lpa, rpa = pts
-            trans = get_neuromag_transform(nasion, lpa, rpa)
-            return trans
-        else:
-            return np.zeros((3, 3))
+        if self.elp_raw is None:
+            return
+        pts = transform_ALS_to_RAS(self.elp_raw[:3])
+        nasion, lpa, rpa = pts
+        trans = get_neuromag_transform(nasion, lpa, rpa)
+        return trans
 
+    @cached_property
     def _get_fid_src(self):
+        if self.elp_raw is None:
+            return np.empty((0, 3))
         pts = self.elp_raw[:3]
-        if np.any(pts):
-            pts = transform_ALS_to_RAS(pts)
-            pts = np.dot(pts, self.neuromag_trans.T)
+        pts = transform_ALS_to_RAS(pts)
+        pts = np.dot(pts, self.neuromag_trans.T)
         return pts
 
+    # cached_property
     def _get_elp_src(self):
+        if self.elp_raw is None:
+            return np.empty((0, 3))
         pts = self.elp_raw[3:]
-        if np.any(pts):
-            pts = transform_ALS_to_RAS(pts)
-            pts = np.dot(pts, self.neuromag_trans.T)
+        pts = transform_ALS_to_RAS(pts)
+        pts = np.dot(pts, self.neuromag_trans.T)
         return pts
 
-    def _get_dev_head_trans(self):
-        if np.any(self.fid_src):
-            src_pts = self.elp_src
+    @cached_property
+    def _get_hsp_src(self):
+        if (self.hsp_raw is None) or not np.any(self.neuromag_trans):
+            return  np.empty((0, 3))
         else:
-            return None
+            pts = transform_ALS_to_RAS(self.hsp_raw)
+            pts = np.dot(pts, self.neuromag_trans.T)
+            return pts
 
-        if np.any(self.mrk):
-            dst_pts = self.mrk
-        else:
-            return None
+    @cached_property
+    def _get_dev_head_trans(self):
+        if (self.mrk is None) or not np.any(self.fid_src):
+            return np.empty((0, 0))
+
+        src_pts = self.elp_src
+        dst_pts = self.mrk
 
         n_use = len(self.use_mrk)
         if n_use < 3:
@@ -137,33 +154,7 @@ class Kit2FiffPanel(HasTraits):
         trans = fit_matched_pts(src_pts, dst_pts, params=False)
         return trans
 
-    def _get_fid_dst(self):
-        if np.any(self.dev_head_trans):
-            return apply_trans(self.dev_head_trans, self.fid_src)
-        else:
-            return np.empty((0, 3))
-
-    def _get_elp_dst(self):
-        if np.any(self.dev_head_trans):
-            return apply_trans(self.dev_head_trans, self.elp_src)
-        else:
-            return np.empty((0, 3))
-
-    def _get_hsp_src(self):
-        if os.path.exists(self.hsp_file):
-            pts = read_hsp(self.hsp_file)
-            pts = transform_ALS_to_RAS(pts)
-            pts = np.dot(pts, self.neuromag_trans.T)
-            return pts
-        else:
-            return np.empty((0, 3))
-
-    def _get_hsp_dst(self):
-        if np.any(self.dev_head_trans) and np.any(self.hsp_src):
-            return apply_trans(self.dev_head_trans, self.hsp_src)
-        else:
-            return np.empty((0, 3))
-
+    @cached_property
     def _get_event_info(self):
         """
         Return a string with the number of events found for each trigger value
@@ -179,19 +170,58 @@ class Kit2FiffPanel(HasTraits):
 
         return os.linesep.join(count)
 
+    @cached_property
+    def _get_can_save(self):
+        can_save = (self.sqd_file and np.any(self.dev_head_trans)
+                    and np.any(self.hsp_src) and np.any(self.elp_src)
+                    and np.any(self.fid_src))
+        return can_save
+
+    def _save_as_fired(self):
+        # find default path
+        path = self.sqd_file[:-3]
+        if not path.endswith('raw'):
+            path += '-raw'
+        path += '.fif'
+
+        # save as dialog
+        dlg = FileDialog(action="save as", wildcard="fiff raw file (*.fif)|*.fif",
+                         default_path=path)
+        dlg.open()
+        if dlg.return_code != OK:
+            return
+        if not path.endswith('.fif'):
+            path += '.fif'
+            if os.path.exists(path):
+                answer = confirm(None, "The file %r already exists. Should it be "
+                                 "replaced?", "Overwrite File?")
+                if answer != YES:
+                    return
+
+        try:
+            raw = RawKIT()
+            raw.save(path)
+        except Exception as err:
+            msg = str(err)
+            error(None, msg, "Kit2Fiff Error")
+            raise
+
     @on_trait_change('scene.activated')
     def _init_plot(self):
         self.fid_obj = PointObject(scene=self.scene, color=(25, 225, 25),
                                    point_scale=5e-3)
-        self.sync_trait('fid_dst', self.fid_obj, 'points', mutual=False)
+        self.sync_trait('fid_src', self.fid_obj, 'points', mutual=False)
+        self.sync_trait('dev_head_trans', self.fid_obj, 'trans', mutual=False)
 
         self.elp_obj = PointObject(scene=self.scene, color=(50, 50, 220),
                                    point_scale=1e-2, opacity=.2)
-        self.sync_trait('elp_dst', self.elp_obj, 'points', mutual=False)
+        self.sync_trait('elp_src', self.elp_obj, 'points', mutual=False)
+        self.sync_trait('dev_head_trans', self.elp_obj, 'trans', mutual=False)
 
         self.hsp_obj = PointObject(scene=self.scene, color=(200, 200, 200),
                                    point_scale=2e-3)
-        self.sync_trait('hsp_dst', self.hsp_obj, 'points', mutual=False)
+        self.sync_trait('hsp_src', self.hsp_obj, 'points', mutual=False)
+        self.sync_trait('dev_head_trans', self.hsp_obj, 'trans', mutual=False)
 
 
 
@@ -231,20 +261,20 @@ class MainWindow(HasTraits):
     """GUI for interpolating between two KIT marker files"""
     scene = Instance(MlabSceneModel, ())
     headview = Instance(HeadViewController)
-    control = Instance(ControlPanel)
+    panel = Instance(ControlPanel)
 
     def _headview_default(self):
         hv = HeadViewController(scene=self.scene, scale=160, system='RAS')
         return hv
 
-    def _control_default(self):
+    def _panel_default(self):
         p = ControlPanel(scene=self.scene)
         return p
 
     view = View(HGroup(Item('scene',
                             editor=SceneEditor(scene_class=MayaviScene)),
                        VGroup(Item('headview', style='custom'),
-                              Item('control', style='custom'),
+                              Item('panel', style='custom'),
                               show_labels=False),
                        show_labels=False,
                       ),
