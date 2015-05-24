14151b6a64492eba34f661cfce7ebd6c509c9b23
Mainak Jas <mainakjas@gmail.com>
2015-05-24 15:38:31 +0200

    Make example work

--- examples/forward/plot_transform.py
+++ examples/forward/plot_transform.py
@@ -1,5 +1,4 @@
 import os.path as op
-import numpy as np
 
 import mne
 from mne.forward import make_field_map, transform_instances
@@ -11,55 +10,27 @@ trans_fname = op.join(path, 'MEG', 'sample', 'sample_audvis_raw-trans.fif')
 subjects_dir = op.join(path, 'subjects')
 
 condition = 'Left Auditory'
-evoked = mne.read_evokeds(evoked_fname, condition=condition, baseline=(-0.2, 0.0))
-
-# Compute the field maps to project MEG and EEG data to MEG helmet
-# and scalp surface
-maps = make_field_map(evoked, trans_fname, subject='sample',
-                      subjects_dir=subjects_dir,  meg_surf='head')
-p_field = evoked.plot_field(maps, time=0.11)
-
-
-p_helmet = mne.viz.plot_trans(evoked.info, trans_fname, 'sample', subjects_dir,
-                              ch_type='meg', coord_frame='meg')
-
+evoked = mne.read_evokeds(evoked_fname, condition=condition,
+                          baseline=(-0.2, 0.0))
 
 # let's assume you have multiple runs and your subject moved :(
 evoked_rot = evoked.copy()
 trans_rot = evoked_rot.info['dev_head_t']['trans']
+trans_rot[:] = mne.transforms.translation(x=0, y=0, z=0.05).dot(trans_rot)
 
-rot = mne.transforms.rotation(x=0, y=np.pi/16, z=0)
-trans_rot[:] = rot.dot(trans_rot)
-
-maps_rot = make_field_map(evoked_rot, trans_fname, subject='sample',
-                          subjects_dir=subjects_dir, meg_surf='head')
-p_field_rot = evoked_rot.plot_field(maps_rot, time=0.11)
-
-p_helmet_rot = mne.viz.plot_trans(evoked_rot.info, trans_fname, 'sample',
-                                  subjects_dir, ch_type='meg',
-                                  coord_frame='meg')
-
-# we can see that the dipoles are now more pronounced on the rotated head
-# bc the magnetic field has changed given the head position
-# let's try to rotate the head
-# we start with the same data in both evokeds
-# the infos are different because of the rotation to the dev_head_t
-# this will result in our simulated rotated data.
-# now same dev_head_t, different data
-
-# let's see if we can return to our original data
-# we apply a rotation in the opposite direction to return to original evoked
-
-trans_rot = evoked.copy().info['dev_head_t']['trans']
-rot = mne.transforms.rotation(x=0, y=-np.pi/16, z=0)
-trans_rot = rot.dot(trans_rot)
-evoked_rot.info['dev_head_t']['trans'] = trans_rot
-
+# now let's put it back in place
+evoked_old = evoked_rot.copy()
 transform_instances([evoked, evoked_rot])
-maps_mod = make_field_map(evoked_rot, trans_fname, subject='sample',
-                          subjects_dir=subjects_dir, meg_surf='head')
-p_field_mod = evoked_rot.plot_field(maps_rot, time=0.11)
-picks = mne.pick_types(evoked.info, meg=True)
 
-# # values are different
-# np.testing.assert_allclose(evoked.data[picks], evoked_rot.data[picks])
+# let's make a before and after map
+maps = make_field_map(evoked_old, trans_fname, subject='sample',
+                      subjects_dir=subjects_dir, meg_surf='helmet')
+evoked_old.plot_field(maps, time=0.11)
+
+evoked_rot = evoked.copy()
+trans_rot = evoked_rot.info['dev_head_t']['trans']
+trans_rot[:] = mne.transforms.translation(x=0, y=0, z=0.05).dot(trans_rot)
+
+maps = make_field_map(evoked_rot, trans_fname, subject='sample',
+                      subjects_dir=subjects_dir,  meg_surf='helmet')
+evoked_rot.plot_field(maps, time=0.11)
